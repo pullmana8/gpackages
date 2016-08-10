cb40933d41d3a5834d1c978970c925fa35a6dc12
Alex Legler <alex@a3li.li>
2016-08-10 13:29:12 +0200

    Remove popover keyword legend, link instead
    Doesn't work properly on XS screens

--- app/assets/javascripts/packages/show.js
+++ app/assets/javascripts/packages/show.js
@@ -9,16 +9,4 @@ $(function() {
   }).fail(function() {
     $('#changelog-container > li').html('<span class="fa fa-fw fa-3x fa-ban text-danger"></span><br><br>Changelog currently not available. Please check back later.');
   });
-
-  $('#kk-keyword-legend-btn').popover({
-    content: function(btn) {
-      return $('#kk-keyword-legend-text').html();
-    },
-    html: true,
-    trigger: 'toggle'
-  });
-
-  $('#kk-keyword-legend-btn').click(function() {
-    return false;
-  });
 });
