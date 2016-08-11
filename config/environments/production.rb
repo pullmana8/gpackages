535409029081486e0990e4307b4b6e7785dbd3f0
Alex Legler <alex@a3li.li>
2016-08-11 22:34:12 +0200

    Send mail via sendmail in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,9 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  # Send mail via sendmail
+  config.action_mailer.delivery_method = :sendmail
+
   # Do not dump schema after migrations.
   # config.active_record.dump_schema_after_migration = false
 end
