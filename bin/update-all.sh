e09ea9a82abef11fa09319491749c7f95f4ef730
Robin H. Johnson <robbat2@gentoo.org>
2017-10-17 10:48:59 -0700

    bin: fix script path.
    Signed-off-by: Robin H. Johnson <robbat2@gentoo.org>

--- bin/update-all.sh
+++ bin/update-all.sh
@@ -6,8 +6,8 @@ else
     git pull --rebase &>/dev/null
 fi
 
-/var/www/packages.gentoo.org/update-md5.sh
-/var/www/packages.gentoo.org/update-use.sh
+/var/www/packages.gentoo.org/htdocs/bin/update-md5.sh
+/var/www/packages.gentoo.org/htdocs/bin/update-use.sh
 
 cd /var/www/packages.gentoo.org/htdocs || exit 1
 bundle exec rake kkuleomi:update:all RAILS_ENV=production &>/dev/null
