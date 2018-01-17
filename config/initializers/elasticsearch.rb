1068321e1477ad7a243eb04e3424faf117c0d1ad
Alec Warner <antarus@gentoo.org>
2018-01-17 09:45:59 -0500

    Allow env control of elasticsearch debugging.
    Turning on debug logs means it will dump ES queries, this is helpful
    when debugging query problems during ES migration.

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,7 +1,7 @@
 require 'elasticsearch/persistence/model'
 
 Elasticsearch::Persistence.client = Elasticsearch::Client.new host: ENV['ELASTICSEARCH_URL'] || 'elasticsearch:9300'
-if Rails.env.development?
+if Rails.env.development? or ENV['RAILS_DEBUG']
   logger           = ActiveSupport::Logger.new(STDERR)
   logger.level     = Logger::DEBUG
   logger.formatter = proc { |s, d, p, m| "\e[2m#{m}\n\e[0m" }
