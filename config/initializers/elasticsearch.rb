0ff50dcc4bcde5ba74565e7891b8ea5437c36279
Alec Warner <antarus@gentoo.org>
2018-02-18 18:08:04 -0500

    Also fix ES name, default to localhost.
    Docker can remap it it with env variables easily.

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,6 +1,6 @@
 require 'elasticsearch/persistence/model'
 
-Elasticsearch::Persistence.client = Elasticsearch::Client.new host: ENV['ELASTICSEARCH_URL'] || 'elasticsearch:9200'
+Elasticsearch::Persistence.client = Elasticsearch::Client.new host: ENV['ELASTICSEARCH_URL'] || 'localhost:9200'
 if Rails.env.development? or ENV['RAILS_DEBUG']
   logger           = ActiveSupport::Logger.new(STDERR)
   logger.level     = Logger::DEBUG
