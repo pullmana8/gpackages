3a72f426ed692ef796a6b16579d8f6cbafbd89de
Alec Warner <antarus@gentoo.org>
2018-04-16 22:32:13 -0400

    Fix bug annotations.
    Instead of showing the entire commit message, look for glep66 headers in
    the commit body and pull out the list of bugs. Then annotate each bug #
    as a link. Then join the links into a list like:
    
    12345,56788,12456
    
    Each number is a link to the bug.

--- app/helpers/packages_helper.rb
+++ app/helpers/packages_helper.rb
@@ -29,6 +29,18 @@ module PackagesHelper
     end.join(' ').html_safe
   end
 
+	# This parses commit messages for GLEP66 style bug annotations.
+	# Bug: https://bugs.gentoo.org/NNNNNN
+	# Closes: https://bugs.gentoo.org/NNNNNN
+	def glep66_bugs(commit_msg)
+		bugs_list = []
+		commit_msg.each_line do |line| {
+			bugno = line[/(Bug\:|Closes\:)\s+https:\/\/bugs\.gentoo\.org\/(\d+)/, 1]
+			bugs_list << "https://bugs.gentoo.org/#{bugno}" if !bugno.nil?
+		}
+		bugs_list
+	end
+
   def annotate_bugs(str)
     annotated_str = str.gsub(/([bB]ug\s+|[bB]ug\s+#|#)(\d+)/) do
       link_to_bug("#{$1}#{$2}", $2)
