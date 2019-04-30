e5fff77c4cb12155f7cffa9a920fae92097ad4af
Antonette Caldwell <antonettecaldwell@tutanota.com>
2019-04-30 16:31:31 -0500

    Update store.rb

--- lib/kkuleomi/store.rb
+++ lib/kkuleomi/store.rb
@@ -14,36 +14,134 @@ module Kkuleomi::Store
 #		]
 
     base_settings = {
-        analysis: {
-            filter: {
-                autocomplete_filter: {
-                    type: 'edge_ngram',
-                    min_gram: 1,
-                    max_gram: 20,
-                }
-            },
-            analyzer: {
-                autocomplete: {
-                    type: 'custom',
-                    tokenizer: 'standard',
-                    filter: %w(lowercase autocomplete_filter)
-                }
-            }
+  "mappings": {
+    "dynamic": "false",
+    "settings": {
+      "number_of_shards": 1,
+      "analysis": {
+        "filter": {
+          "autocomplete_filter": {
+            "type": "edge_ngram",
+            "min_gram": 2,
+            "max_gram": 10
+          }
         },
-	#			index: { mapper: { dynamic: false } },
-	#			mapping: { total_fields: { limit: 50000 } }
+        "analyzer": {
+          "autocomplete": {
+            "type": "custom",
+            "tokenizer": "standard",
+            "filter": [
+              "lowercase",
+              "autocomplete_filter"
+            ]
+          }
+        },
+        "index": {
+          "mapping": {
+            "total_fields": {
+              "limit": 50000
+            }
+          }
+        },
+        "properties": {
+          "package": {
+            "properties": {
+              "category": {
+                "type": "string",
+                "analyzer": "autocomplete",
+                "search_analyzer": "standard",
+                "fields": {
+                  "raw": {
+                    "type": "string"
+                  },
+                  "english": {
+                    "type": "text",
+                    "analyzer": "english"
+                  }
+                }
+              },
+              "name": {
+                "type": "string",
+                "fields": {
+                  "raw": {
+                    "type": "string"
+                  }
+                }
+              },
+              "homepace": {
+                "type": "string",
+                "fields": {
+                  "raw": {
+                    "type": "string"
+                  }
+                }
+              },
+              "licenses": {
+                "type": "string",
+                "fields": {
+                  "raw": {
+                    "type": "string"
+                  }
+                }
+              },
+              "herds": {
+                "type": "string",
+                "fields": {
+                  "raw": {
+                    "type": "string"
+                  }
+                }
+              },
+              "metadata_hash": {
+                "type": "string",
+                "fields": {
+                  "raw": {
+                    "type": "string"
+                  }
+                }
+              }
+            }
+          },
+          "category": {
+            "name": {
+              "type": "text"
+            },
+            "description": {
+              "type": "text"
+            },
+            "metadata_hash": {
+              "type": "text"
+            }
+          },
+          "change": {
+            "package": {
+              "type": "keyword"
+            },
+            "category": {
+              "type": "keyword"
+            },
+            "change_type": {
+              "type": "keyword"
+            },
+            "version": {
+              "type": "keyword"
+            },
+            "arches": {
+              "type": "keyword"
+            },
+            "commit": {
+              "type": "objecT",
+              "fields": {
+                "hash": {
+                  "type": "murmur3"
+                }
+              }
+            }
+          }
+        }
+      }
     }
-
-		# In ES 1.5, we could use 1 mega-index. But in ES6, each model needs its own.
-#		types.each { |type|
-	# Gateway is deprecated
-#						client = type.gateway.client
-#						client.indices.delete(index: type.index_name) rescue nil if force
-#						body = {
-#							settings: type.settings.to_hash.merge(base_settings),
-#						 	mappings: type.mappings.to_hash
-#						}
-#						client.indices.create(index: type.index_name, body: body)
-#		}
+  }
+}
   end
 end
