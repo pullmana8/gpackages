ec4e593685dd838d9eb2e2cb594ad1482925a8c6
Antonette Caldwell <antonettecaldwell@tutanota.com>
2019-04-30 11:53:01 -0500

    Update store.rb

--- lib/kkuleomi/store.rb
+++ lib/kkuleomi/store.rb
@@ -1,16 +1,17 @@
 module Kkuleomi::Store
-  def self.refresh_index
-    Category.gateway.refresh_index!
-  end
+#  def self.refresh_index
+#    Category.gateway.refresh_index!
+#  end
+# refresh_index is only available under Elasticsearch Persistence under a class, not a module
 
-  def self.create_index(force = false)
-		types = [
-			Category,
-			Package,
-			Version,
-			Change,
-			Useflag,
-		]
+#  def self.create_index(force = false)
+#		types = [
+#			Category,
+#			Package,
+#			Version,
+#			Change,
+#			Useflag,
+#		]
 
     base_settings = {
         analysis: {
@@ -29,19 +30,20 @@ module Kkuleomi::Store
                 }
             }
         },
-				index: { mapper: { dynamic: false } },
-				mapping: { total_fields: { limit: 50000 } }
+	#			index: { mapper: { dynamic: false } },
+	#			mapping: { total_fields: { limit: 50000 } }
     }
 
 		# In ES 1.5, we could use 1 mega-index. But in ES6, each model needs its own.
-		types.each { |type|
-						client = type.gateway.client
-						client.indices.delete(index: type.index_name) rescue nil if force
-						body = {
-							settings: type.settings.to_hash.merge(base_settings),
-						 	mappings: type.mappings.to_hash
-						}
-						client.indices.create(index: type.index_name, body: body)
-		}
+#		types.each { |type|
+	# Gateway is deprecated
+#						client = type.gateway.client
+#						client.indices.delete(index: type.index_name) rescue nil if force
+#						body = {
+#							settings: type.settings.to_hash.merge(base_settings),
+#						 	mappings: type.mappings.to_hash
+#						}
+#						client.indices.create(index: type.index_name, body: body)
+#		}
   end
 end
