b7f4dfb3a5db1c0df58cfb1a357f9651a7a1fb9f
Hans de Graaff <graaff@gentoo.org>
2019-07-22 13:36:25 +0200

    Use the fixture repo in the test environment
    Signed-off-by: Hans de Graaff <graaff@gentoo.org>

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,3 +40,5 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
+
+KKULEOMI_PORTDIR = Rails.root.join('test', 'fixtures', 'repo')
