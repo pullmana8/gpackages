a94249d041d11948ae058b866d4b4ef91a437796
Hans de Graaff <graaff@gentoo.org>
2019-07-22 12:27:34 +0200

    Remove category tests for unimplemented methods
    Signed-off-by: Hans de Graaff <graaff@gentoo.org>

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -7,39 +7,14 @@ class CategoriesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:categories)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create category" do
-    assert_difference('Category.count') do
-      post :create, category: {  }
-    end
-
-    assert_redirected_to category_path(assigns(:category))
-  end
-
   test "should show category" do
     get :show, id: @category
     assert_response :success
   end
 
-  test "should get edit" do
-    get :edit, id: @category
+  test "should search category" do
+    get :search, id: 'virtual'
     assert_response :success
   end
 
-  test "should update category" do
-    patch :update, id: @category, category: {  }
-    assert_redirected_to category_path(assigns(:category))
-  end
-
-  test "should destroy category" do
-    assert_difference('Category.count', -1) do
-      delete :destroy, id: @category
-    end
-
-    assert_redirected_to categories_path
-  end
 end
